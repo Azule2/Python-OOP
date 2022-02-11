108386bea32f2c270164ed12e8d2c9411fe89861
Azule2 <69428406+Azule2@users.noreply.github.com>
2022-02-11 11:54:17 +0300

    Create Car.py

---
+++ Car.py
@@ -0,0 +1,11 @@
+class Car:
+
+    def __init__(self):
+        pass
+
+    def attribute(self):
+        price = 0
+        color = None
+        speed = 0
+    def method(self):
+        print('Car moveing')
